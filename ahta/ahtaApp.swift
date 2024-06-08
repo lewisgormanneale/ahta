9c6c47b133721427a0cf92e1cff8a4a60904e43f
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-06-08 16:48:27 +0100

    initialise ios app project

---
+++ ahta/ahtaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ahtaApp.swift
+//  ahta
+//
+//  Created by Lewis Gorman-Neale on 08/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ahtaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
