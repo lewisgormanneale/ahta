b9ac5a862487f5661bbcc069795542e290b7a02c
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-08-11 23:19:41 +0100

    add list of tasks

--- ahta/ContentView.swift
+++ ahta/ContentView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let tasks = ["Task 1", "Task 2", "Task 3"]
+
     var body: some View {
         ZStack {
-            Color(.green)
+            Color.green
                 .ignoresSafeArea()
             VStack {
                 Text("ahta")
                     .font(.largeTitle)
                     .fontWeight(.semibold)
                 Text("another habit tracking app")
+                ZStack {
+                   List {
+                        ForEach(tasks, id: \.self) { task in
+                            Text(task)
+                        }
+                    }
+                    .scrollContentBackground(.hidden)
+                }
             }
         }
     }
